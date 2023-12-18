23ea819f3222257f005927e6d3e4f67a7841a2a7
sellylima <vanessa.limanasc@gmail.com>
2023-12-18 11:14:51 -0300

    arquivos iniciais do projeto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+//  PRECISA CHAMAR UM POR UM??
+const metricas =  document.querySelectorAll('metrica') 
+
+const textArea = document.getElementById('user-input')
+const resetButton = document.getElementById('reset-button')
+
+resetButton.addEventListener('click', () => {
+    textArea.value = '';
+    metricas.textContent = 'palavras';
+
+})
