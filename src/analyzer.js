ac8b9eeedc3da68a4967974f197d4925559edb92
sellylima <vanessa.limanasc@gmail.com>
2024-01-11 13:34:33 -0300

    adicionei o projeto Text  Analyze

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,34 @@
-const analyzer = {  
-  getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+const analyzer = {
+  //esta função deve retornar a contagem de palavras encontradas no parâmetro `text` do tipo `string`. //calcular a contagem de palavras de um texto.
+  getWordCount: (textArea) => {
+    return textArea.split(" ").length;
   },
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
+    //Esta função deve retornar a contagem de caracteres encontrados no parâmetro `text` do tipo `string`.
   },
-  getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
+   getCharacterCountExcludingSpaces: (text) => {
+  // esta função deve retornar a contagem de caracteres excluindo espaços e sinais de pontuação encontrados no parâmetro `text` do tipo `string`.
+   },
+   getAverageWordLength: (text) => {
+  //TODO: Esta função deve retornar o comprimento médio das palavras encontradas no parâmetro `text` do tipo `string`.
+   },
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+  // Esta função deve retornar quantos números estão no parâmetro `text` do tipo `string`.
   },
   getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+  //Esta função deve retornar a soma de todos os números encontrados no parâmetro `text` do tipo `string`.
+  const somaNumeros = text.match(/\d+/g);
+  if(numbers){
+    const sum =  numbers.reduce((acc, num) => acc + parseInt (num), 0) ;
+    return sum;
+  }else{
+    return 0;
+  }
+
+ // return numbers ? numbers.reduce((acc, num) => acc + parseInt (num), 0) : 0;
+  //let total = somaNumeros.reduce((total,somaNumeros) => total + somaNumeros, 0);
+  //console.log(total);
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-import analyzer from './analyzer.js';
+import analyzer from "./analyzer.js";
+//TODO: ouve eventos DOM e invoca os métodos do objeto `analyzer`
+//declarando todos os itens
+const textArea = document.querySelector('[name="user-input"]'); // esse Dom seleciona o atributo name igual ao user-input
+const button = document.getElementById('[reset-button"]');
+const wordCount = document.querySelector('[data-testid="word-count"]'); //retornar a contagem de palavras encontradas no parametro text
+const characterCount = document.querySelector('[data-testid="character-count"]'); //contar caracteres
+const characterNoSpacesCount = document.querySelector('[data-testid="character-no-spaces-count"]'); //contar caracteres excluindo espaços
+const numberCount = document.querySelector('[data-testid="number-count"]'); //elemento Dom para contar os numeros no texto
+const numberSum = document.querySelector('[data-testid="number-sum"]'); //elemento Dom exibir  a soma dos numeros do texto
+const wordLengthAvarege = document.querySelector('[data-testid="word-length-average"]'); //exibir a media de comprimento das palavras
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+//evento textArea 
+// addEventListener - usar sempre apos seletor de Dom
+textArea.addEventListener("keyup", () => {
+  const inputText = textArea.value;
+  wordCount.textContent = analyzer.getWordCount(textArea);
+  characterCount.textContent = analyzer.getCharacterCount(textArea);
+  analyzer.getCharacterCountExcludingSpaces(textArea);
+  numberCount.textContent = analyzer.getNumberCount(textArea);
+  numberSum.textContent = analyzer.getNumberSum(textArea);
+});
 
-//  PRECISA CHAMAR UM POR UM??
-const metricas =  document.querySelectorAll('metrica') 
+//Reset Botão
+const resetButton = document.getElementById("reset-button");
 
-const textArea = document.getElementById('user-input')
-const resetButton = document.getElementById('reset-button')
+resetButton.addEventListener("click", () => {
+  textArea.value = " ";
+});
 
-resetButton.addEventListener('click', () => {
-    textArea.value = '';
-    metricas.textContent = 'palavras';
-
-})
+function clean() {
+  textArea.value = " textArea";
+}
